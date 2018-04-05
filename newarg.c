e3910df322834ef5399ae93a4bc63f1fe2d7f13a
George Gaarder <ggaarder@foxmail.com>
2018-04-05 16:25:04 +0800

    use a new plain text format of network representation

--- newarg.c
+++ newarg.c
@@ -3,19 +3,31 @@
 #include <time.h>
 #include "ntwkarg.h"
 
-int main() {
-  FILE *fp = fopen(ARG, "w");
-  struct header h = {2, 20, 28*28, 10};
-  int i;
-  int argsiz = ARGSIZ;
-  fwrite(&h, sizeof(struct header), 1, fp);
-  float *arg = malloc(sizeof(float)*argsiz);
-  
-  srand(time(0));
-  for (i = 0; i < argsiz; ++i)
-    arg[i] = (float)rand()/RAND_MAX;
-  fwrite(arg, sizeof(float), argsiz, fp);
+#define HNECNT 20 // hidden layer neuron count
+#define CHARN 10 // number of characters to recognize, that is the number of neurons of the output layer
+#define RND ((float)rand()/RAND_MAX)
 
+int main() {
+  FILE *fp = fopen(ARGFN, "w");
+  int i, j, l;
+  int L = 6;
+  int K = 28*28;
+  fprintf(fp, "Number of Layers: %d\nNumber of Input Layers: %d\n", L, K);
+
+  srand(time(0));
+  for (l = 1; l < L; ++l) {  
+    fprintf(fp, "--- Layer %d %s\n", l, (l == L-1 ? "(Output Layer)" : "(Hidden Layer)"));
+    int ncnt = (l == L-1 ? CHARN : HNECNT);
+    fprintf(fp, "Number of Neurons: %d\n", ncnt);
+    int wcnt = (l == 1 ? K : HNECNT);
+    fprintf(fp, "Number of Weights: %d\n", wcnt);
+    for (i = 0; i < ncnt; ++i) {
+      fprintf(fp, "---- Neuron %d of Layer %d\n", i, l);
+      for (j = 0; j < wcnt; ++j)
+        fprintf(fp, "%f ", RND);
+      fprintf(fp, "%f\n", RND);
+    }
+  }
+ 
   fclose(fp);
-  free(arg);
 }
--- ntwkarg.h
+++ ntwkarg.h
@@ -1,25 +1,6 @@
 #ifndef _NNMNIST_CONF_H
 #define _NNMNIST_CONF_H
 
-#define ARG "ntwkarg"
-
-struct header {
-  int L; // hidden layers
-  int N; // neurons per a hidden layer
-  int K; // input neurons, 28*28 grayscale. and that would be the count of the weights
-  int D; // neurons of the output layout
-};
-
-#define NEUSIZ (h.N*(h.K+1)) // +1 means bias
-#define HLASIZA (h.N*NEUSIZ)  // hidden layer size
-#define HLASIZ (h.L*HLASIZA)
-#define OLASIZ (h.D*NEUSIZ)    // output layer size
-#define ARGSIZ (HLASIZ+OLASIZ)
-
-// weight, l = 0..L, n = 0..N-1, k = 0..K-1
-// l = L means the output layer, where n = 0..D-1
-#define WEIG (arg+l*HLASIZA+n*NEUSIZ+k)
-// bias
-#define BIAS HLW(l, n, h.K)
+#define ARGFN "ntwkarg" // arg filename
 
 #endif /* _NNMNIST_CONF_H */
