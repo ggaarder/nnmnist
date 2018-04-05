fe5d1eccc56411ce74e16e84d9d911a4be4e0cd7
George Gaarder <ggaarder@foxmail.com>
2018-04-06 07:13:55 +0800

    typo + do not specify type byte long in the format specification

--- nnmnist.c
+++ nnmnist.c
@@ -1,35 +1,35 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <math.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "ntwkarg.h"
+#include <time.h>
+
+#define NTWKFN "ntwkarg" // network arguments storage filename
 
 #if 0
 // Network Storage Format
 
-uint32 L; // number of layers, at least 3, that is one input layer, one hidden layer and one output layer
+int L; // number of layers, at least 3, that is one input layer, one hidden layer and one output layer
 
 struct layer {
-    uint32 ncnt; // number of neurons. 0 for the input layer
+    int ncnt; // number of neurons. 0 for the input layer
     
     struct neuron {
-        float32 w[layers[l-1].ncnt]; // weights. number of weights is the number of the neurons of the last layer
-        float32 bias;
+        float w[layers[l-1].ncnt]; // weights. number of weights is the number of the neurons of the last layer
+        float bias;
     } neurons[ncnt];
 } layers[L];
 #endif
-
-#define ARGFN "ntwkarg" // arg filename
 
 #define LBL "train-labels.idx1-ubyte"
 #define IMG "train-images.idx3-ubyte"
 char *lbl, *img;
 
-uint32_t toggledn(uint32_t value) // Change Endianness
-{
-    uint32_t result = 0;
+int toggledn(int value) { // Change Endianness
+    int result = 0;
     result |= (value & 0x000000FF) << 24;
     result |= (value & 0x0000FF00) << 8;
     result |= (value & 0x00FF0000) >> 8;
@@ -38,15 +38,13 @@ uint32_t toggledn(uint32_t value) // Change Endianness
 }
 
 float sigm(float x) {
-  x = 1.0 + exp(x);
+  x = 1.0 + exp(-x);
   return 1.0/x;
 }
 
 float dsigm(float x) {
-  float ex = exp(x);
-  x = 1.0+1.0/ex;
-  x = ex*x*x;
-  return 1.0/x;
+  x = exp(-x);
+  return x/(1.0+x)/(1.0+x);
 }
 
 int getlbl(int no) { // no starts from 0
@@ -60,7 +58,42 @@ void getimg(int no) { // no starts from 0
 }
 
 int main() {
-  int i;
+  int ntwkfd = open(NTWKFN, O_RDWR);
+  if (errno == ENOENT) {
+    printf("Creating new network storage at %s\n", NTWKFN);
+    ntwkfd = open(NTFKFN, O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR);
+
+    int L = 4+2;
+    write(fd, &L, sizeof(int));
+    int ncnt = 28*28;
+    write(fd, &ncnt, sizeof(int));
+    int wcnt;
+    int l, n, i;
+    srand(time(0));
+    float v;
+    
+    for (l = 1; l < L-1; ++l) {
+      wcnt = ncnt;
+      ncnt = 20;
+      for (n = 0; n < ncnt; ++n)
+        for (i = 0; i <= wcnt; ++i) { // together with bias
+          v = (float)rand()/RAND_MAX;
+          write(fd, &v, sizeof(float));
+        }
+    }
+
+    wcnt = ncnt;
+    ncnt = 10; // recognize 10 digits
+    for (n = 0; n < ncnt; ++n)
+      for (i = 0; i <= wcnt; ++i) { // together with bias
+        v = (float)rand()/RAND_MAX;
+        write(fd, &v, sizeof(float));
+      }    
+        
+    close(ntwkfd);
+    return 0;
+  }
+
   int lblfd = open(LBL, O_RDONLY);
   int imgfd = open(IMG, O_RDONLY);
   int lbllen = lseek(lblfd, 0, SEEK_END)+1;
@@ -71,26 +104,23 @@ int main() {
   int xcnt = toggledn(*(uint32_t*)(lbl+4));
   printf("%d Training Samples\n", xcnt);
   
-  while (1) {
-    int imgno;
+  int imgno;
 
-    for (imgno = 0; imgno < xcnt; ++imgno) {
-      int i;
-      int l, n, k;
-
-      for (l = 0; l <= h.L; ++l) {
-        for (n = 0; n < h.N; ++n) {
-          float *zat = z+NEUOFS
-            *zat = *BIAS;
-          for (k = 0; k < h.K; ++k)
-            *zat += (*WEIG)*in[k];
-          a[NEUOFS] = sigm(*zat);
-        }
+  for (imgno = 0; imgno < xcnt; ++imgno) {
+    int i;
+    int l, n, k;
+    
+    for (l = 0; l <= h.L; ++l) {
+      for (n = 0; n < h.N; ++n) {
+        float *zat = z+NEUOFS
+          *zat = *BIAS;
+        for (k = 0; k < h.K; ++k)
+          *zat += (*WEIG)*in[k];
+        a[NEUOFS] = sigm(*zat);
       }
     }
   }
 
-  for (i = 0; i < LYRCNT; ++i) free(neuofs[i]);
   munmap(lbl, lbllen);
   munmap(img, imglen);
 }
